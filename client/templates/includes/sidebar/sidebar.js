f2b8b6c484a9b6e197a45c26bec9606dfc769c7b
t <tschultztt@gmail.com>
2014-11-09 11:39:22 -0600

    Fixed the sidebar so on screen resize the sidebar remains at 100%

--- client/templates/includes/sidebar/sidebar.js
+++ client/templates/includes/sidebar/sidebar.js
@@ -1,6 +1,8 @@
 Template.sidebar.rendered = function () {
   //Makes sure the sidebars hieght is always 100%
-  Tracker.autorun(function () {
+  $("#sidebar").height(Math.max($("body").height(),$("#sidebar").height()));
+  $( window ).resize(function() {
     $("#sidebar").height(Math.max($("body").height(),$("#sidebar").height()));
   });
+
 };
